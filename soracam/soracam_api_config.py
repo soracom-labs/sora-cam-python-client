6f962525151b5bdddab40803c0fb4798a7e5ee4d
Ash Igarashi <igarashi@soracom.jp>
2023-05-18 15:02:40 +0900

    initial commit

--- soracam/soracam_api_config.py
+++ soracam/soracam_api_config.py
@@ -1,5 +1,9 @@
 
+<<<<<<< HEAD
 REQUESTS_TIMEOUT = 10
+=======
+REQUESTS_TIMEOUT = 60
+>>>>>>> c0561ac (initial commit)
 LOOP_WAITE_TIME = 10  # wait time for the loop
 WAITE_TIMEOUT = 900  # wait until 900 seconds
 SORA_CAM_BASE_URL = 'v1/sora_cam/devices'
